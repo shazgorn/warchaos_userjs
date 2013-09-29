0824e8253998ebf279951530f678cfce6f552efd
shazgorn <shazgorn@yandex.ru>
2013-09-29 20:04:14 +0400

    astar, fix for ap_calc

--- wc_ap_calc.user.js
+++ wc_ap_calc.user.js
@@ -12,48 +12,43 @@
  * time when each AP will be restored
  */
 
-(function () {
+(function() {
 	/**
 	 * Description: convert minutes to hh:mm format
 	 * Return: string representing time in "hh:mm" format
 	 */
-	'use strict';
 	function convertMinutesToHM(timeInMinutes) {
-		var res, h = Math.floor(timeInMinutes / 60), m = Math.floor(timeInMinutes % 60);
-		if (timeInMinutes === 1440) {
-			res = "24:00";
-		} else {
+		if (timeInMinutes == 1440)
+			return "24:00";
+		else
 			timeInMinutes %= 1440;
-			if (h < 10) {
-				h = "0" + h;
-			}
-			if (m < 10) {
-				m = "0" + m;
-			}
-			res = h + ":" + m;
-		}
-		return res;
+		var h = Math.floor(timeInMinutes/60);
+		if (h < 10)
+			h = "0" + h;
+		var m = Math.floor(timeInMinutes%60);
+		if (m < 10)
+			m = "0" + m;
+		return h + ":" + m;
 	}
 	/**
 	 * Description: create table containing AP regeneration time
 	 * Return: raw table
 	 */
 	function createTable(cur, max) {
-		var t = "", timeToRegenOnePoint = Math.floor(1440 / max),
-			d = new Date(), minutesSinceMidnight = d.getMinutes() + d.getHours() * 60,
-			ceil = Math.ceil(cur),
-			mileStoneTime = minutesSinceMidnight,
-			i;
+		var t = "";
+		var timeToRegenOnePoint = Math.floor(1440 / max);
 		t += "Время восст. 1 ед.: " + convertMinutesToHM(timeToRegenOnePoint);
-		if (cur === max) {
+		if (cur == max)
 			return t;
-		}
 		t += "<table><tr><td>Ходы</td><td>Время</td><td>Через</td></tr>";
-		if (cur === ceil) {
-			ceil++;
-			cur++;
+		var d = new Date(),
+			minutesSinceMidnight = d.getMinutes() + d.getHours() * 60,
+			ceil = Math.ceil(cur);
+		if (cur == ceil) {
+			ceil++; cur++;
 		}
-		for (i = 0; ceil < max + 1; i++) {
+		var mileStoneTime = minutesSinceMidnight;
+		for (var i = 0; ceil < max + 1; i++) {
 			t += "<tr>";
 			if (ceil > max) {
 				t += "<td>" + max + "</td>";
@@ -63,7 +58,7 @@
 			t += "<td>";
 			if (i === 0 && cur < ceil && ceil <= max) {
 				//when part of ap will be regenerated
-				mileStoneTime += Math.floor((ceil - cur) * timeToRegenOnePoint);
+				mileStoneTime += Math.floor((ceil-cur) * timeToRegenOnePoint);
 			} else if (ceil > max) {
 				mileStoneTime += Math.floor((max - parseInt(max, 10)) * timeToRegenOnePoint);
 			} else {
@@ -79,14 +74,14 @@
 		return t;
 	}
 	function calculateAP() {
-		var fonts = document.getElementsByTagName("font"), i, m, t;
+		var fonts = document.getElementsByTagName("font");
 		if (fonts) {
-			for (i = 0; i < fonts.length; i++) {
-				if (fonts[i].innerHTML.search("Ходы") !== -1) {
+			for (var i = 0; i < fonts.length; i++) {
+				if (fonts[i].innerHTML.search("Ходы") != -1) {
 					if (!fonts[i].parentNode.nextSibling.hasAttribute("tooltip")) {
-						m = fonts[i].parentNode.nextSibling.innerHTML.match(/([\d+\.]+) \/ ([\d+\.]+)/);
+						var m = fonts[i].parentNode.nextSibling.innerHTML.match(/(.+) \/ (.+)/);
 						if (m) {
-							t = createTable(parseFloat(m[1]), parseFloat(m[2]));
+							var t = createTable(parseFloat(m[1]), parseFloat(m[2]));
 							fonts[i].parentNode.nextSibling.setAttribute("tooltip", t);
 						}
 					}
@@ -94,13 +89,12 @@
 			} //for
 		} //if
 	}
-	(function (f) {
+	(function(f) {
 		var wc_ifr = document.getElementById("ifr");
-		if (wc_ifr) {
-			wc_ifr.addEventListener("load", function () {
+		if (wc_ifr) 
+			wc_ifr.addEventListener("load", function() {
 				setTimeout(f, 0);
 			}, false);
-		}
 		f();
-	}(calculateAP));
-}());
+	})(calculateAP);	
+})();
