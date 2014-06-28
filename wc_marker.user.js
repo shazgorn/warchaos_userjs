38a637321fb39a95082fa9d1d783e272022cbabf
Shazgorn <shazgorn@yandex.ru>
2014-06-29 03:03:53 +0400

    market 20h to 25h

--- wc_marker.user.js
+++ wc_marker.user.js
@@ -3,22 +3,23 @@
 // @namespace      https://github.com/shazgorn/warchaos_userjs
 // @description    alt + click works as shift + click
 // @match          http://warchaos.ru/f/a
-// @version        1.1
+// @version        1.2
 // @downloadURL    https://raw.github.com/shazgorn/warchaos_userjs/master/wc_marker.user.js
 // ==/UserScript==
 
 
 addEventListener("click", function(e) {
-	if (e.altKey && e.target == "[object HTMLImageElement]" && e.target.hasAttribute("sck"))
-		eval(e.target.getAttribute("sck"));
-	if (e.altKey || e.shiftKey || e.button == 2) {
-		setTimeout(function() {
-			var input = document.getElementById("miniinfoy");
-			if (input !== null) {
-				if (input.childNodes[0].getAttribute("value") === "0") {
-					input.selectedIndex = 6;
-				}
-			}
-		}, 1000);
-	}
+    if (e.altKey && e.target == "[object HTMLImageElement]" && e.target.hasAttribute("sck"))
+        eval(e.target.getAttribute("sck"));
+    if (e.altKey || e.shiftKey || e.button == 2) {
+        setTimeout(function() {
+            var input = document.getElementById("miniinfoy");
+            if (input !== null) {
+                if (input.childNodes[0].getAttribute("value") === "0") {
+                    input.selectedIndex = 6;
+                    document.getElementById('sx0').childNodes[0].setAttribute('value', '25');
+                }
+            }
+        }, 1000);
+    }
 }, false);
