5d2bfa911b221a1425e6cf20477ec9a288d2e6af
Shazgorn <shazgorn@yandex.ru>
2014-07-05 21:01:29 +0400

    icon replacer lite

--- wc_icon_replacer.user.js
+++ wc_icon_replacer.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name           Warchaos Icon Replacer
+// @name           Warchaos Icon Replacer Lite
 // @namespace      https://github.com/shazgorn/warchaos_userjs
-// @description    Replace default recipe icon(1914.gif). Add art info
+// @description    Replace default recipe icon(1914.gif). Add art info. lite version
 // @match          http://warchaos.ru/*
-// @version        1.21
-// @downloadURL    https://raw.github.com/shazgorn/warchaos_userjs/master/wc_icon_replacer.user.js
+// @version        1.00
 // ==/UserScript==
 
+
+// downloadURL    https://raw.github.com/shazgorn/warchaos_userjs/master/wc_icon_replacer.user.js
 (function() {
     // return;
     function source() {
@@ -145,45 +146,6 @@
                     }
                 }
             }//for
-            safeNames();
-        }
-        function safeNames() {
-            if ($('.safe-name').length === 0 && window.g) {
-                $("<style>.safe-name {position: relative; bottom: 5px; font-size: 10px; color: yellow; font-family: Arial;} </style>").appendTo("head");
-                townId = "safe_" + window.g.mobjects[window.g.objcity + 5];
-                safes = localStorage.getItem(townId);
-                if (safes === null || safes === "") {
-                    safes = "";
-                    $("img[src='2618.gif']").each(function(i, safe) {
-                        safes += "Сейф" + i + ",";
-                    });
-                    localStorage.setItem(townId, safes);
-                }
-                safesArray = safes.split(",");
-                localStorage.setItem("safes", "");
-                // dropdown menu give to
-                var j = 0;
-                $("select.mtext option").each(function(i, safe) {
-                    if ($(safe).html() === "Сейф") {
-                        $(safe).html(safesArray[j++]);
-                    }
-                });
-                // take from
-                $("img[src='2618.gif']").each(function(i, safe) {
-                    var span = document.createElement('span');
-                    $(span).html(safesArray[i]);
-                    $(span).addClass('safe-name');
-                    $(safe).parent().parent().append(span);
-                    $(span).click(function() {
-                        var name = prompt("Введите название сейфа", $(this).html());
-                        if (name) {
-                            $(this).html(name.substr(0, 8));
-                            safesArray[i] = $(this).html();
-                            localStorage.setItem(townId, safesArray.join());
-                        }
-                    });
-                });
-            }
         }
         (function(f) {
             addEventListener('click', function() {
