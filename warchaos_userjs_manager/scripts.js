7fb9d76655b797efad522672d4acac2bea7771b6
Shazgorn <shazgorn@yandex.ru>
2014-09-05 04:14:26 +0400

    quick fix

--- warchaos_userjs_manager/scripts.js
+++ warchaos_userjs_manager/scripts.js
@@ -1,7 +1,9 @@
-console.log('scripts start');
-var scripts = [
-    {name: "icon_replacer", events: ["frame_load", "click"]},
-    {name: "replace_force_size", events: ["frame_load", "click"]}
-];
-window.scripts = scripts;
-console.log('scripts stop');
+(function() {
+    console.log('scripts start');
+    var scripts = [
+        {name: "icon_replacer", events: ["frame_load", "click"]},
+        {name: "replace_force_size", events: ["frame_load", "click"]}
+    ];
+    window.scripts = scripts;
+    console.log('scripts stop');
+})();
