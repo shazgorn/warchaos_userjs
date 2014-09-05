6cc6d49591a2802862e4cfccefb2f89167e22b27
Shazgorn <shazgorn@yandex.ru>
2014-09-05 18:49:17 +0400

    quick fix

--- warchaos_userjs_manager/wc_manager.user.js
+++ warchaos_userjs_manager/wc_manager.user.js
@@ -6,82 +6,70 @@
 // @version        1.00
 // ==/UserScript==
 function f() {
-    function addScript(src) {
+    /*
+     * scr - script source url
+     * onload - handler to fire on load event
+     */
+    function addScript(src, onload) {
         var docscripts = document.getElementsByTagName('script');
         for (var i = 0; i < docscripts.length; i++) {
-            if (docscripts[i].getAttribute('src') == src)
+            if (docscripts[i].getAttribute('src') === src)
                 return;
         }
         var script = document.createElement('script');
         script.src = src;
         document.head.appendChild(script);
+        if (onload) {
+            script.addEventListener('load', onload, false);
+        }
+        return script;
     }
-    function init() {
-        console.log(0);
-        if (typeof u === 'undefined') {
-            u = document.getElementById('cise');
+    (function() {
+        var basepath;
+        if (navigator.appVersion.search('Chrome') === -1) {
+            basepath = "https://raw.githubusercontent.com/shazgorn/warchaos_userjs/master/warchaos_userjs_manager/";
+        } else {
+            basepath = "https://rawgit.com/shazgorn/warchaos_userjs/master/warchaos_userjs_manager/";
         }
-        if (typeof $ === 'undefined') {
-            addScript('http://code.jquery.com/jquery-1.9.1.js');
-        }
-        if (typeof $ === 'undefined') {
-            console.log('a');
-            setTimeout(init, 100);
-            return;
-        }
-        console.log(1);
-        if (typeof scripts === 'undefined') {
-            console.log(2)
-            var script = document.createElement('script');
-            var basepath = 'https://raw.githubusercontent.com/shazgorn/warchaos_userjs/master/warchaos_userjs_manager/';
-            var scrname = 'scripts';
-            console.log(3)
-            script.src = basepath + scrname + '.js';
-            console.log(4)
-            document.body.appendChild(script);
-            script.addEventListener('load', function() {
-                console.log('script load');
-                console.log(scripts);
-                init2();
-            }, false)
-            console.log(5)
-            console.log(window.scripts);
-            console.log(6);
-            setTimeout(init, 100);
-            return;
-        }
-        console.log(7);
-        function init2() {
-            for (var i = 0; i < scripts.length; i++) {
-                var script = document.createElement('script');
-                script.src = basepath + scripts[i].name + '.js';
-                console.log(1);
-                var ifr = window.frames['ifr'];
-                if (ifr) {
-                    var name = scripts[i].name;
-                    for (var j = 0; j < scripts[i].events.length; j++) {
-                        switch (scripts[i].events[j]) {
-                            case 'click':
-                                window.addEventListener('click', function() {
-                                    eval(name + '()');
-                                }, false);
-                                break;
-                            case 'load':
-                                break;
-                            case 'frame_load':
-                                ifr.addEventListener('load', function() {
-                                    //eval(name + '()');
-                                }, false);
-                                break;
+        // жабаскрипт головного мозга
+        addScript("http://yastatic.net/jquery/2.1.1/jquery.min.js", function() {
+            addScript(basepath + 'scripts.js', function() {
+                for (var i = 0; i < scripts.length; i++) {
+                    if (scripts[i].match) {
+                        for (var j = 0; j < scripts[i].match.length; j++) {
+                            console.log(scripts[i].match[j].charAt(-1));
+                        }
+                    } else {
+
+                    }
+                    addScript(basepath + scripts[i].name + '.js');
+                    var ifr = window.frames['ifr'];
+                    if (ifr) {
+                        var name = scripts[i].name;
+                        for (var j = 0; j < scripts[i].events.length; j++) {
+                            switch (scripts[i].events[j]) {
+                                case 'click':
+                                    window.addEventListener('click', function() {
+                                        eval(name + '()');
+                                    }, false);
+                                    break;
+                                case 'load':
+                                    break;
+                                case 'frame_load':
+                                    ifr.addEventListener('load', function() {
+                                        eval(name + '()');
+                                    }, false);
+                                    break;
+                            }
                         }
                     }
                 }
-                document.body.appendChild(script);
-            }
-        }
-    }
-
-    init();
+            });
+        });
+    })();
+}
+if (typeof u === 'undefined') {
+    u = document.getElementById('cise');
 }
 var script = document.createElement('script');
 script.textContent = '(' + f + ')();';
