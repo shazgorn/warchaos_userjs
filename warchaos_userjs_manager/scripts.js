1c6a59f0e099cbae41cadd806335b93c05c5e72c
Shazgorn <shazgorn@yandex.ru>
2014-09-05 19:13:16 +0400

    quick fix

--- warchaos_userjs_manager/scripts.js
+++ warchaos_userjs_manager/scripts.js
@@ -1,4 +1,4 @@
 var scripts = [
-    {name: "icon_replacer", events: ["frame_load", "click"]},
+    {name: "icon_replacer", events: ["frame_load", "click"], match: ['http://warchaos.ru/*']},
 //    {name: "replace_force_size", events: ["frame_load", "click"]}
 ];
